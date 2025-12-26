23fbf822e3caf6832c166bb70ad8f6744ed86a06
saufle1x <illyakuzenko1@gmail.com>
2025-12-26 11:22:33 +0200

    Add script to disable memory compression
    This script disables memory compression using PowerShell.

---
+++ memorycompression.cmd
@@ -0,0 +1,19 @@
+Made by Saufle1x
+
+@echo off
+echo 
+color 5
+cls
+echo.
+echo        optimization made by:
+echo         saufle1x
+echo.
+echo.
+echo Starting in 1 second
+
+PowerShell "Disable-MMAgent -MemoryCompression"
+
+@echo.
+@echo.
+@echo Completed
+@pause
